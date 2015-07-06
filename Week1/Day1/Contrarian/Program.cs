14dadfd6e07e451534c7457f1202bb716f933498
dani frederick <dandiellie@yahoo.com>
2015-07-06 17:50:38 -0500

    Finished Contrarian

--- Week1/Day1/Contrarian/Program.cs
+++ Week1/Day1/Contrarian/Program.cs
@@ -11,46 +11,102 @@ namespace Contrarian
         static void Main(string[] args)
         {
             string statement = "";
+            string output = "";
+            bool play = false;
             
             //welcome statement
             Console.WriteLine("I am the Contrarian. Tell me something you like or dislike.");
 
             do
             {
-                Console.WriteLine("Enter a statement that begins 'I like...' or 'I don't like...'");
-                statement = Console.ReadLine();
-                int n = statement.Length;
+                do
+                {
+                    //accept a statement
+                    Console.WriteLine("Enter a statement that begins 'I like...' or 'I don't like...'\n");
+                    statement = Console.ReadLine();
 
-                if (IsLike(statement))
-                {
-                    break;
-                }
-                else if (IsDislike(statement))
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid format! Please try again!");
-                }
-            } while (true);
+                    //The following lines were included for debugging.
+                    //Console.WriteLine(RemoveWords(statement, 4));
+                    //break;
+
+                    if (IsLike(statement))
+                    {
+                        //if the user likes something do this
+                        output = "I don't like " + RemoveWords(statement, 2);
+                        break;
+                    }
+                    else if (IsDislike(statement))
+                    {
+                        //if the user dislikes something do this
+                        output = "I like " + RemoveWords(statement, 3);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nInvalid format! Please try again!");
+                    }
+                } while (true);
+
+                Console.WriteLine("\n" + output);
+
+                Console.WriteLine("Would you like to exit? Enter '1' to CONTINUE or '0' to EXIT.");
+                play = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
+            } while (play);
+
+            Console.WriteLine("Goodbye!");
+            Console.ReadLine();
         }
 
-        public bool IsLike(string s)
+        //checks if a statement expresses like and is in the right format
+        static bool IsLike(string s)
         {
-            using (StringReader myReader = new StringReader(s))
+            char delimiter = ' ';
+            string[] words = s.Split(delimiter);
+            int l = words.GetLength(0);
+
+            if (words[0]=="I" && words[1]=="like")
             {
-                //empty!!! This is a mess!!
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //checks if a statement expresses dislike and is in the right format
+        static bool IsDislike(string s)
+        {
+            char delimiter = ' ';
+            string[] words = s.Split(delimiter);
+            int l = words.GetLength(0);
+
+            if (words[0]=="I" && words[1]=="don't" && words[2]=="like")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //removes n words from the beginning of a string
+        static string RemoveWords(string s, int n)
+        {
+            char delimiter = ' ';
+            string[] words = s.Split(delimiter);
+            int l = words.GetLength(0);
+
+            //clear string s
+            s = "";
+
+            for (int i = n; i < l; i++)
+            {
+                s = s + words[i] + " ";
             }
 
-            return false;
-        }
-
-        public bool IsDislike(string s)
-        {
-            //empty method!!
-
-            return false;
+            return s;
         }
     }
 }
