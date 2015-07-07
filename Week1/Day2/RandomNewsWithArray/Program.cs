fcb38463e3ac75f579be1f816e6d5032900d1591
dani frederick <dandiellie@yahoo.com>
2015-07-07 11:59:31 -0500

    Individual Project RandomNewsWithArray finished.

---
+++ Week1/Day2/RandomNewsWithArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNewsWithArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random myRand = new Random();
+            int selection = myRand.Next(3);
+            string[] headlines = {"Martians Attack!", "Astros win World Series!", "Perry the Platypus has been found!"};
+
+            Console.WriteLine("TODAY'S HEADLINE\n");
+            Console.WriteLine(headlines[selection]);
+            Console.ReadLine();
+        }
+    }
+}
