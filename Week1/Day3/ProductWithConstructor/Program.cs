14a7f1e83e3faecaf8c7e5f2fb56ce1fe797fa65
dani frederick <dandiellie@yahoo.com>
2015-07-08 10:06:49 -0500

    In class exercise ProductWithConstructor finished. Practiced using constructors.

---
+++ Week1/Day3/ProductWithConstructor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductWithConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int UnitsInStock { get; set; }
+        public string Description { get; set; }
+
+        public Product(string name, decimal price, int unitsInStock)
+        {
+            Name = name;
+            Price = price;
+            UnitsInStock = unitsInStock;
+        }
+    }
+}
