2cd2c5dd7656f3a90b0543c993c883dbb2c04064
dani frederick <dandiellie@yahoo.com>
2015-07-09 11:25:11 -0500

    In class practice...

---
+++ Week1/Day4/Classwork/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwork
+{
+    class Program
+    {
+        static void DoSomething(string value)
+        {
+            value = "Goodbye!";
+        }
+
+        static void Increment(int n)
+        {
+            int m = n;
+            n++;
+
+            Debug.Assert(n == m+1, "The incremented integer should be: " + m);
+        }
+
+        static void Main(string[] args)
+        {
+            var message1 = "Hello!";
+            DoSomething(message1);
+            Console.WriteLine("The value of message1 is " + message1);
+
+            Increment(5);
+
+            // pause
+            Console.ReadLine();
+        }
+    }
+}
