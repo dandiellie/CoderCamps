535b8e22f62ba0117396202e0fd81ecb4949ec71
dani frederick <dandiellie@yahoo.com>
2015-07-11 23:14:08 -0500

    Homework assignment OrgChart finished.

---
+++ Week1/Day5/OrgChart/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrgChart
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Employee DirectBoss { get; set; }
+        public List<Employee> Minions { get; set; }
+
+        public Employee(string first, string last)
+        {
+            FirstName = first;
+            LastName = last;
+            Minions = new List<Employee>();
+        }
+
+        public void PrintEmployeeInfo()
+        {
+            try
+            {
+                Console.WriteLine(string.Format("\n{0} {1} reports to {2} {3} and has the following employee(s) under him/her:", FirstName, LastName, DirectBoss.FirstName, DirectBoss.LastName));
+            }
+            catch
+            {
+                Console.WriteLine(string.Format("\n{0} {1} reports to (none) and has the following employee(s) under him/her:", FirstName, LastName));
+            }
+            
+            foreach (Employee minion in Minions)
+            {
+                Console.Write(string.Format("{0} {1} ", minion.FirstName, minion.LastName));
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Week1/Day5/OrgChart/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrgChart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee bill = new Employee("Bill", "Mason");
+            Employee sam = new Employee("Sam", "Garner");
+            Employee fred = new Employee("Fred", "Gallagher");
+            Employee jane = new Employee("Jane", "Faucet");
+            Employee alice = new Employee("Alice", "Moreau");
+
+            bill.Minions.Add(sam);
+            bill.Minions.Add(fred);
+            bill.Minions.Add(jane);
+            sam.DirectBoss = bill;
+            fred.DirectBoss = bill;
+            jane.DirectBoss = bill;
+
+            fred.Minions.Add(alice);
+            alice.DirectBoss = fred;
+
+            bill.PrintEmployeeInfo();
+            sam.PrintEmployeeInfo();
+            fred.PrintEmployeeInfo();
+            jane.PrintEmployeeInfo();
+            alice.PrintEmployeeInfo();
+
+            Console.ReadLine();             //pause
+        }
+    }
+}
