c3ae5e54ede5c5c950c5be9ab94c85126b332da7
dani frederick <dandiellie@yahoo.com>
2015-07-13 11:55:29 -0500

    Additional in class exercises working with exceptions.

---
+++ Week2/Day1/CreateBitmap/Program.cs
@@ -0,0 +1,33 @@
+﻿// The following code was given in the Coder Camps online textbook.
+// The assignment was to fix and improve it.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CreateBitmap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create bitmap
+            var bitmap = new Bitmap(500, 500);
+
+            // randomly set 1000 pixels
+            var rnd = new Random();
+            for (var i = 0; i < 1000; i++)
+            {
+                bitmap.SetPixel(rnd.Next(500), rnd.Next(500), Color.Red);
+            }
+
+            // save bitmap to current directory (debug directory)
+            var currentDirectory = Environment.CurrentDirectory;
+            var path = Path.Combine(currentDirectory, "MyImage.png");
+            bitmap.Save(path, ImageFormat.Png);
+        }
+    }
+}
---
+++ Week2/Day1/FlakyCatch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlakyCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random myRand = new Random();
+            
+            try
+            {
+                Flaky(myRand);
+                Console.WriteLine("Flaky was called successfully.");
+            }
+            catch
+            {
+                Console.WriteLine("Yikes! There was an error.");
+            }
+            finally
+            {
+                Console.WriteLine("Finished calling Flaky.");
+            }
+
+            Console.ReadLine();  //pause
+        }
+
+        static void Flaky(Random r)
+        {
+            int choice = r.Next(2);
+            if(choice==0)
+            {
+                throw new Exception("Haha! You lose!");
+            }
+        }
+    }
+}
