1beb43e43817200274b5120fb1aa5cc4c5744519
dani frederick <dandiellie@yahoo.com>
2015-07-13 09:43:44 -0500

    About 40 - 80 milliseconds faster.

--- Week2/Day1/PerformanceWithStopwatch/Program.cs
+++ Week2/Day1/PerformanceWithStopwatch/Program.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace MyConsoleApp
 {
@@ -23,18 +24,30 @@ namespace MyConsoleApp
             var stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            // Display count of duplicates
-            foreach (var number1 in numberList)
+            //// Display count of duplicates (Original Version)
+            //foreach (var number1 in numberList)
+            //{
+            //    var duplicateCount = 0;
+            //    foreach (var number2 in numberList)
+            //    {
+            //        if (number1 == number2)
+            //        {
+            //            duplicateCount++;
+            //        }
+            //    }
+            //    Console.WriteLine("{0} has {1} duplicates.", number1, duplicateCount);
+            //}
+
+            // Display count of duplicates (New Version)
+            var q = from x in numberList
+                    group x by x into g
+                    let count = g.Count()
+                    orderby count descending
+                    select new { Name = g.Key, Count = count };
+
+            foreach (var x in q)
             {
-                var duplicateCount = 0;
-                foreach (var number2 in numberList)
-                {
-                    if (number1 == number2)
-                    {
-                        duplicateCount++;
-                    }
-                }
-                Console.WriteLine("{0} has {1} duplicates.", number1, duplicateCount);
+                Console.WriteLine("{0} has {1} duplicates.", x.Name, x.Count);
             }
 
             // Stop Stopwatch
