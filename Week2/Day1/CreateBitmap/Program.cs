9aa9cfeaf3ca965015ee3d00c8cdba56b7fdedf6
dani frederick <dandiellie@yahoo.com>
2015-07-13 14:02:35 -0500

    Finished CreateBitmap. Practiced using statement to close with IDisposable interface.

--- Week2/Day1/CreateBitmap/Program.cs
+++ Week2/Day1/CreateBitmap/Program.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace CreateBitmap
 {
@@ -15,19 +17,22 @@ namespace CreateBitmap
         static void Main(string[] args)
         {
             // create bitmap
-            var bitmap = new Bitmap(500, 500);
-
-            // randomly set 1000 pixels
-            var rnd = new Random();
-            for (var i = 0; i < 1000; i++)
+            using (Bitmap bitmap = new Bitmap(500, 500))
             {
-                bitmap.SetPixel(rnd.Next(500), rnd.Next(500), Color.Red);
+                // randomly set 1000 pixels
+                var rnd = new Random();
+                for (var i = 0; i < 1000; i++)
+                {
+                    bitmap.SetPixel(rnd.Next(500), rnd.Next(500), Color.Green);
+                }
+
+                // save bitmap to current directory (debug directory)
+                string currentDirectory = Environment.CurrentDirectory;
+                string path = Path.Combine(currentDirectory, "MyImage2.png");
+                bitmap.Save(path, ImageFormat.Png);
             }
 
-            // save bitmap to current directory (debug directory)
-            var currentDirectory = Environment.CurrentDirectory;
-            var path = Path.Combine(currentDirectory, "MyImage.png");
-            bitmap.Save(path, ImageFormat.Png);
+            Console.ReadLine();
         }
     }
 }
